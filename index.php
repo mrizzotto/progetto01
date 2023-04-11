8ab50e7407a9ed075b1488a0a9ac18979bc4f3c8
mrizzotto <rizzotto@libero.it>
2023-04-11 20:52:54 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+$server = "https://webstudenti.unimarconi.it/phpMyAdmin";
+$utente = "ma.rizzotto";
+$password = "J16TriH4";
+$database = "ma_rizzotto";
+
+$connessione = mysqli_connect($server , $utente , $password , $database);
+
+
+?>
